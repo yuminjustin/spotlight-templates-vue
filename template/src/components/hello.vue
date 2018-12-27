2bdce4db7a285340ebfda85dbf517368951c0d4e
justin lee <yuminjustin@163.com>
2018-12-27 14:52:29 +0800

    Update hello.vue

--- template/src/components/hello.vue
+++ template/src/components/hello.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div :class="style.hello">
       <h1>Hello, Vue!</h1>
       <img src="/static/image/white.png">
   </div>
 </template>
-<style>
-  h1 {
-    color:green;
-  }
-</style>
+<script>
+import style from "B/assets/css/page.css"
+export default {
+    data(){
+      return {
+        style
+      }
+    }
+}
+</script>
